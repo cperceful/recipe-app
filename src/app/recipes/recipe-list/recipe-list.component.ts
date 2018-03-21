9e71a3c56f1d21fca216c55e44942579d777ba94
cperceful <cpercef1@gmail.com>
2018-03-21 18:21:14 -0500

    Basics of recipe list built

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
-  
+  recipes: Recipe[] = [
+    new Recipe('Toast', 'Like bread, but hot', 'https://upload.wikimedia.org/wikipedia/commons/3/37/Toast-2.png')
+  ];
+
   constructor() { }
 
   ngOnInit() {
